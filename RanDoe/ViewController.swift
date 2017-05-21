975d0c2d8f7435a6a4a4e0da57a94e0c014ec404
Jeff Tabios <jeff@givit.com>
2017-05-21 01:23:38 -0700

    Added Internet monitoring and pop-up alert

--- RanDoe/ViewController.swift
+++ RanDoe/ViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import AFNetworking
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Pop Alert for Internet Warning
+        let alertController = UIAlertController(title: "Internet is gone", message: "Please check your internet", preferredStyle: .Alert)
+        
+        let defaultAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alertController.addAction(defaultAction)
+        
+        
+        //Internet Watch
+        AFNetworkReachabilityManager.sharedManager().setReachabilityStatusChangeBlock { (status: AFNetworkReachabilityStatus) -> Void in
+            switch status {
+            case .NotReachable:
+                self.presentViewController(alertController, animated: true, completion: nil)
+                print("Not reachable")
+            case .ReachableViaWiFi, .ReachableViaWWAN:
+                print("Reachable")
+            case .Unknown:
+                print("Unknown")
+            }
+            
+        }
+        AFNetworkReachabilityManager.sharedManager().startMonitoring()
+
+        
     }
 
     override func didReceiveMemoryWarning() {
